273ab8a47bf235b5604c0a791ca4b3744281660a
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-11-12 10:23:09 +0300

    Redirect to home after login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -59,7 +59,7 @@ const login = async (req, res) => {
             // console.log(req.user);
             // console.log(token);
             // return res.redirect('/api/orders/addSticker');
-            return res.redirect('/api/stickers/getStickersByTags')
+            return res.redirect('/api/stickers/')
             
             // res.status(200).json({message: "Logged in successfully!", user: user})
 
