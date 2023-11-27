580534901e14b0fcef629b2a8283efaa1776a3ba
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-11-27 10:20:21 +0300

    Clean up

--- src/middlewares/jwtCookie.js
+++ src/middlewares/jwtCookie.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 
 exports.jwtCookies = (req, res, next) => {
-    // const token2 = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MzdjNTZiOTdkMTk0ODI5MTFmZDc0ZCIsImVtYWlsIjoibWFoZGVydGVrb2xhQGdtYWlsLmNvbSIsInVzZXJuYW1lIjoib29nd2F5cnVsZXMiLCJmaXJzdE5hbWUiOiJNYWhpZGVyIiwibGFzdE5hbWUiOiJPb2d3YXkiLCJpYXQiOjE2OTg4MjE2NDAsImV4cCI6MzQwMjgyNzI4MH0.NwLcRedqZiljkFnMbeaLIPmoyx0KfBONQ4DzWpojHDk`;
+    
 
     try{
         const token = req.cookies.token;
